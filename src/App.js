ce5bb875f297a6f24ba49dc1663a08b1c06c8efd
Johny <suprotikkvcdutta@gmail.com>
2021-07-31 18:42:46 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,45 @@
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import { useEffect, React,useState } from 'react';
+import Pusher from 'pusher-js';
+import axios from './axios'
 
 function App() {
+  const [messages,setMessages] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/messages/sync')
+    .then(response=>{
+      
+      setMessages(response.data)
+    })
+  },[])
+
+  useEffect(() => {
+    const pusher = new Pusher('676afaf045b40eb9faf8', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage)=> {
+      setMessages([...messages,newMessage])
+    });
+
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+    
+  }, [messages]);
+
+  console.log(messages)
+
   return (
     <div className="app">
        <div className='app__body'>
           <Sidebar/>
-          <Chat/>
+          <Chat messages={messages}/>
         </div>
       
     </div>
